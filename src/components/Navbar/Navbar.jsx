ab8d8979bc47088f128cb036d9d06514e5b8e7a4
RyanKarakoc <kochy@hotmail.co.uk>
2023-06-06 11:50:32 +0100

    created nav bar template

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 id="nav">HOME</h1>
+      <h1 id="nav">ABOUT</h1>
+      <h1 id="nav">PROJECTS</h1>
+      <h1 id="nav">RESUME</h1>
+      <h1 id="nav">CONTACT</h1>
+    </div>
+  );
+};
+
+export default Navbar;
