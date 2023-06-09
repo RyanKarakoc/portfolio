ac4079be2171666bbc74437d349c47d6bcf171f1
RyanKarakoc <kochy@hotmail.co.uk>
2023-06-09 11:30:57 +0100

    added navigation to projects

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,6 +10,13 @@ const Navbar = () => {
     }
   };
 
+  const handleProjectsClick = () => {
+    const element = document.getElementById("projectImg1Status");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
+
   return (
     <div className="navbar">
       <h1 id="nav" onClick={scrollToTop}>
@@ -18,7 +25,9 @@ const Navbar = () => {
       <h1 id="nav" onClick={handleAboutClick}>
         ABOUT
       </h1>
-      <h1 id="nav">PROJECTS</h1>
+      <h1 id="nav" onClick={handleProjectsClick}>
+        PROJECTS
+      </h1>
       <h1 id="nav">RESUME</h1>
       <h1 id="nav">CONTACT</h1>
     </div>
