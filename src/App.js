1578d630f2e518d04f7dc4bde9e893497da18c5d
RyanKarakoc <kochy@hotmail.co.uk>
2023-06-09 13:30:41 +0100

    added contact template and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
