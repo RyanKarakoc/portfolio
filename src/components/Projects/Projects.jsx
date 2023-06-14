c524534144ceb236fa441a5a15b934d1d2832634
RyanKarakoc <kochy@hotmail.co.uk>
2023-06-14 09:57:05 +0100

    removed unused project space

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,32 +17,32 @@ const Projects = () => {
         </p>
         <img src={gameProject} alt="" id="projectImg1" />
         <div id="projectImg1Status">In Progress</div>
-        <h2 id="projectImg2Header">Project 2</h2>
-        <p id="projectImg2Description">
-          A little bit of imformation regading project 2
-        </p>
-        <img src="" alt="" id="projectImg2" />
-        <div id="projectImg2Status">In Progress</div>
-        <h2 id="projectImg3Header" onClick={handleProject3OnClick}>
+        <h2 id="projectImg2Header" onClick={handleProject3OnClick}>
           Allotment
         </h2>
-        <p id="projectImg3Description">
+        <p id="projectImg2Description">
           Mobile app to help users plant veg at home using notifications for
           watering or seasonal harvest / sowing dates etc..
         </p>
         <img
           src={allotment}
           alt=""
-          id="projectImg3"
+          id="projectImg2"
           onClick={handleProject3OnClick}
         />
-        <div id="projectImg3Status">Completed</div>
+        <div id="projectImg2Status">Completed</div>
+        {/* <h2 id="projectImg3Header">Project 3</h2>
+        <p id="projectImg3Description">
+          A little bit of imformation regading project 3
+        </p>
+        <img src="" alt="" id="projectImg3" />
+        <div id="projectImg3Status">In Progress</div>
         <h2 id="projectImg4Header">Project 4</h2>
         <p id="projectImg4Description">
           A little bit of imformation regading project 4
         </p>
         <img src="" alt="" id="projectImg4" />
-        <div id="projectImg4Status">In Progress</div>
+        <div id="projectImg4Status">In Progress</div> */}
       </div>
     </div>
   );
